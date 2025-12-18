be850c7c157aa52dc371bdec6b42782a46a355b3
yamaya.kohei <yamaya.kohei@raccoon.ne.jp>
2025-12-18 13:08:18 +0900

    cdの設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/components-props/",
   build: {
-    outDir: 'build',
+    outDir: "build",
   },
   server: {
     watch: {
       usePolling: true,
     },
   },
-})
+});
